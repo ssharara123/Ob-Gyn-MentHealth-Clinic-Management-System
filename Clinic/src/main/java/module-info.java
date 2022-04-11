f6d36b2c80570be45deb4f642f26346087e27800
ssharara123 <sharara99@gmail.com>
2022-04-12 00:59:55 +0600

    edit

--- Clinic/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.clinic {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.clinic to javafx.fxml;
-    exports com.example.clinic;
-}
